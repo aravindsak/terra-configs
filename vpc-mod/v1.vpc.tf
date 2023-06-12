b535d49e4d9b75b1cff14c5c9f8e98639ef8b345
aravindsak <aravindk.ark@gmail.com>
2023-06-12 08:16:18 +0530

    nat-gateway and tags

--- vpc-mod/v1.vpc.tf
+++ vpc-mod/v1.vpc.tf
@@ -8,4 +8,18 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # nat gateways
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # tags
+    public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+    private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
 }
